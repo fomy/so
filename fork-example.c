a582f0ee4fed1d1263bf0aacd54bf11b3424153f
fomy <fumin@hust.edu.cn>
2014-11-27 10:03:23 +0800

    how fork() and copy-on-write work

---
+++ fork-example.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/sysinfo.h>
+
+/*
+ * See
+ * http://stackoverflow.com/questions/27161412/does-fork-use-copy-on-write#
+ */
+
+void printMemStat(){
+    struct sysinfo si;
+    sysinfo(&si);
+    printf("===\n");
+    printf("Total: %llu\n", si.totalram);
+    printf("Free: %llu\n", si.freeram);
+}
+
+int main(){
+    long len = 200000000;
+    long *array = malloc(len*sizeof(long));
+    long i = 0;
+    for(; i<len; i++){
+        array[i] = i;
+    }
+
+    printMemStat();
+    if(fork()==0){
+        /*child*/
+        printMemStat();
+
+        i = 0;
+        for(; i<len/2; i++){
+            array[i] = i+1;
+        }
+
+        printMemStat();
+
+        i = 0;
+        for(; i<len; i++){
+            array[i] = i+1;
+        }
+
+        printMemStat();
+
+    }else{
+        /*parent*/
+        int times=10;
+        while(times-->0){
+            printf("sleep in parent %d\n", times);
+            sleep(1);
+        }
+    }
+    return 0;
+}
+
