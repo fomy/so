f6b4168e6cd3b9b4a9d51dc9f655dc194cbf0c49
fomy <fumin@hust.edu.cn>
2016-06-14 14:25:07 +0800

    mq

--- ipc/mq_test.c
+++ ipc/mq_test.c
@@ -1,10 +1,37 @@
 #include <mqueue.h>
 
+// gcc mq_test.c -o mq_test -lrt
+
 int main(int argc, char** argv)
 {
 
-	int mq_fd = open("/tmp/mq", O_CREAT, S_IRUSR|S_IWUSR);
+	mqd_t mq_fd = mq_open("/tmp/mq", O_CREAT|O_EXCL, S_IRUSR|S_IWUSR);
 
-	close(mq_fd);
+	if (mq_fd < 0) {
+		perror("Fail to create mq");
+		assert(mq_fd == -1);
+		if (errno == EEXIST) {
+			mq_fd = mq_open("/tmp/mq", O_WRONLY);
+			if (mq_fd < 0) {
+				perror("Fail to open mq");
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+	}
+
+	/*char *msg = "this is msg from Min";*/
+	/*mq_send(mq_fd, msg, strlen(msg)+1, 0);*/
+
+
+	struct mq_attr mqstat;
+	bzero(mqstat, sizeof(mqstat));
+	int ret = mq_getattr(mq_fd, &mqstat);
+
+	printf("%ld, %ld, %ld, %ld\n", mqstat.mq_flasg, mqstat.mq_maxmsg,
+			mqstat.mq_msgsize, mqstat.mq_curmsgs);
+
+	mq_close(mq_fd);
 	return 0;
 }
